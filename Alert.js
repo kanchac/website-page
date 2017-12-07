ac6c37cf09d8fd155b99f7745a0438110cee39b2
kanchan <kanchanchaudhari94@gmail.com>
2017-12-07 20:51:38 +0530

    script changes implemented

--- Alert.js
+++ Alert.js
@@ -1,4 +1,14 @@
-function 
+function login()
 {
+	var x = document.forms["myForm"]["firstname"].value;
+	var y = document.forms["myForm"]["lastname"].value;
+    if (x == "" || y == "") 
+    {
+        alert("Name must be filled out");  
+    }   
+    else
+    {
+    	alert("Welcome!You have successfully logged in!");
+    }
 
 }
